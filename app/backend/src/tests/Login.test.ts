cb68ff0782f1af0d8ba179ed45e9ab9381d89e25
Frederico Leles Santos <fred.leles@hotmail.com>
2022-10-03 20:36:04 -0300

    tests: Cria um teste para a rota Login

---
+++ app/backend/src/tests/Login.test.ts
@@ -0,0 +1,74 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/User';
+import mocks from './mocks/userMocks';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Login test', () => {
+
+  let chaiHttpResponse: Response;
+
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('Tests if the response gets a token with a valid user', async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(mocks.validUser);
+
+    chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+        email: 'user@user.com',
+        password: 'secret_user',
+      });
+
+    expect(chaiHttpResponse.status).to.be.eq(200);
+    expect(chaiHttpResponse.body).to.have.property('token');
+  });
+  
+  it('Tests if the response gets a error message with an invalid email', async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(undefined);
+
+    chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+        email: 'invalid@user.com',
+        password: 'secret_user',
+      });
+
+    expect(chaiHttpResponse.status).to.be.eq(401);
+    expect(chaiHttpResponse.body.message).to.eq('Incorrect email or password');
+  });
+  
+  it('Tests if the response gets a error message with an invalid password', async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(undefined);
+
+    chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+        email: 'user@user.com',
+        password: 'secret_invalid',
+      });
+
+    expect(chaiHttpResponse.status).to.be.eq(401);
+    expect(chaiHttpResponse.body.message).to.eq('Incorrect email or password');
+  });
+});
