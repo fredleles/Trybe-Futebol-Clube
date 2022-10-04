37c61f72bcc3fc41986264cf14bb3d9e89246c08
Frederico Leles Santos <fred.leles@hotmail.com>
2022-10-03 23:19:39 -0300

    refactor: refatora o arquivo de testes

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -8,126 +8,181 @@ import User from '../database/models/User';
 import mocks from './mocks/userMocks';
 
 import { Response } from 'superagent';
-import TokenHandler from '../utils/TokenHandler';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Login test', () => {
+  describe('Valid User', () => {
 
-  let chaiHttpResponse: Response;
+    let chaiHttpResponse: Response;
+    before(() => {
+      sinon.stub(User, 'findOne').resolves(mocks.validUser);
+    });
+  
+    after(()=>{
+      (User.findOne as sinon.SinonStub).restore();
+    });
+  
+    it('Tests if the response gets a token with a valid user', async () => {
+  
+      chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send({
+          email: 'user@user.com',
+          password: 'secret_user',
+        });
+  
+      expect(chaiHttpResponse.status).to.be.eq(200);
+      expect(chaiHttpResponse.body).to.have.property('token');
+    });
+  });
 
-  after(()=>{
-    (User.findOne as sinon.SinonStub).restore();
-    (TokenHandler.Verify as sinon.SinonStub).restore();
-  })
+  describe('Invalid email', () => {
 
-  it('Tests if the response gets a token with a valid user', async () => {
-    sinon
-      .stub(User, "findOne")
-      .resolves(mocks.validUser);
+    let chaiHttpResponse: Response;
+    before(() => {
+      sinon.stub(User, 'findOne').resolves(undefined);
+    });
+  
+    after(()=>{
+      (User.findOne as sinon.SinonStub).restore();
+    });
 
-    chaiHttpResponse = await chai
+    it('Tests if the response gets a error message with an invalid email', async () => {
+  
+      chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send({
+          email: 'invalid@user.com',
+          password: 'secret_user',
+        });
+  
+      expect(chaiHttpResponse.status).to.be.eq(401);
+      expect(chaiHttpResponse.body.message).to.eq('Incorrect email or password');
+    });
+  });
+  
+  describe('invalid password', () => {
+
+    let chaiHttpResponse: Response;
+    before(() => {
+      sinon.stub(User, 'findOne').resolves(mocks.validUser);
+    });
+  
+    after(()=>{
+      (User.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Tests if the response gets a error message with an invalid password', async () => {
+  
+      chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send({
+          email: 'user@user.com',
+          password: 'secret_invalid',
+        });
+  
+      expect(chaiHttpResponse.status).to.be.eq(401);
+      expect(chaiHttpResponse.body.message).to.eq('Incorrect email or password');
+    });
+  });
+  
+  describe('Invalid request body - email', () => {
+
+    let chaiHttpResponse: Response;
+    before(() => {
+      sinon.stub(User, 'findOne').resolves();
+    });
+  
+    after(()=>{
+      (User.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Tests if the response gets a error message with a request without email', async () => {
+  
+      chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send({
+          email: 'user@user.com',
+        });
+  
+      expect(chaiHttpResponse.status).to.be.eq(400);
+      expect(chaiHttpResponse.body.message).to.eq('All fields must be filled');
+    });
+  });
+
+  describe('Invalid request body - password', () => {
+    let chaiHttpResponse: Response;
+    before(() => {
+      sinon.stub(User, 'findOne').resolves();
+    });
+  
+    after(()=>{
+      (User.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Tests if the response gets a error message with a request without password', async () => {
+  
+      chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send({
+          password: 'secret_invalid',
+        });
+  
+      expect(chaiHttpResponse.status).to.be.eq(400);
+      expect(chaiHttpResponse.body.message).to.eq('All fields must be filled');
+    });
+  });
+  
+  describe('Validate token', () => {
+    let chaiHttpResponse: Response;
+    before(() => {
+      sinon.stub(User, 'findOne').resolves(mocks.validUser);
+    });
+  
+    after(()=>{
+      (User.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Tests if the response gets the right role with a valid token', async () => {
+  
+      chaiHttpResponse = await chai
         .request(app)
-        .post('/login')
-        .send({
-        email: 'user@user.com',
-        password: 'secret_user',
-      });
-
-    expect(chaiHttpResponse.status).to.be.eq(200);
-    expect(chaiHttpResponse.body).to.have.property('token');
-  });
+        .get('/login/validate')
+        .set('Authorization', 'token')
+        .send();
   
-  it('Tests if the response gets a error message with an invalid email', async () => {
-    sinon
-      .stub(User, "findOne")
-      .resolves(undefined);
+      expect(chaiHttpResponse.status).to.be.eq(200);
+      expect(chaiHttpResponse.body.role).to.eq('user');
+    });
+  });
 
-    chaiHttpResponse = await chai
+  describe('Not authorized', () => {
+    let chaiHttpResponse: Response;
+    before(() => {
+      sinon.stub(User, 'findOne').resolves();
+    });
+  
+    after(()=>{
+      (User.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Tests if the response gets an error message when requested without a token', async () => {
+      chaiHttpResponse = await chai
         .request(app)
-        .post('/login')
-        .send({
-        email: 'invalid@user.com',
-        password: 'secret_user',
-      });
-
-    expect(chaiHttpResponse.status).to.be.eq(401);
-    expect(chaiHttpResponse.body.message).to.eq('Incorrect email or password');
+        .get('/login/validate')
+        .send();
+  
+      expect(chaiHttpResponse.status).to.be.eq(401);
+      expect(chaiHttpResponse.body.message).to.eq('Token is mandatory');
+    });
   });
   
-  it('Tests if the response gets a error message with an invalid password', async () => {
-    sinon
-      .stub(User, "findOne")
-      .resolves(mocks.validUser);
-
-    chaiHttpResponse = await chai
-        .request(app)
-        .post('/login')
-        .send({
-        email: 'user@user.com',
-        password: 'secret_invalid',
-      });
-
-    expect(chaiHttpResponse.status).to.be.eq(401);
-    expect(chaiHttpResponse.body.message).to.eq('Incorrect email or password');
-  });
-  
-  it('Tests if the response gets a error message with a request without email', async () => {
-    sinon
-      .stub(User, "findOne")
-      .resolves(mocks.validUser);
-
-    chaiHttpResponse = await chai
-        .request(app)
-        .post('/login')
-        .send({
-        email: 'user@user.com',
-      });
-
-    expect(chaiHttpResponse.status).to.be.eq(400);
-    expect(chaiHttpResponse.body.message).to.eq('All fields must be filled');
-  });
-  
-  it('Tests if the response gets a error message with a request without password', async () => {
-    sinon
-      .stub(User, "findOne")
-      .resolves(mocks.validUser);
-
-    chaiHttpResponse = await chai
-        .request(app)
-        .post('/login')
-        .send({
-        password: 'secret_invalid',
-      });
-
-    expect(chaiHttpResponse.status).to.be.eq(400);
-    expect(chaiHttpResponse.body.message).to.eq('All fields must be filled');
-  });
-  
-  it('Tests if the response gets the right role with a valid token', async () => {
-    sinon
-      .stub(TokenHandler, "Verify")
-      .resolves(mocks.loggedUser);
-
-    chaiHttpResponse = await chai
-      .request(app)
-      .get('/login/validate')
-      .set('Authorization', 'token')
-      .send();
-
-    expect(chaiHttpResponse.status).to.be.eq(200);
-    expect(chaiHttpResponse.body.role).to.eq('user');
-  });
-  
-  it('Tests if the response gets an error message when requested without a token', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .get('/login/validate')
-      .send();
-
-    expect(chaiHttpResponse.status).to.be.eq(401);
-    expect(chaiHttpResponse.body.message).to.eq('Token is mandatory');
-  });
 });
